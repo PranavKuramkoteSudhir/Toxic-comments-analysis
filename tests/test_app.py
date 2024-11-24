35961c37f88f840e60008664c8d21deed2729cc0
PranavKuramkoteSudhir <pranavkuramkotesudhir@gmail.com>
2024-11-24 09:33:10 -0800

    added project root dir to pytest script

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
 import pytest
+import os
+import sys
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_root)
+
 from app import app
 
 @pytest.fixture
