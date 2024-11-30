57d7bde84883832ba029a1107e6ca47d592c30e1
Pranav Kuramkote Sudhir <139109363+PranavKuramkoteSudhir@users.noreply.github.com>
2024-11-29 16:40:06 -0800

    Update setup.py
    test pipleine

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages, setup
 from typing import List
 
-
 def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as file_obj:
